7603480d8663d6c7289e661cf0395d9a67f2226e
2210465185 <2210465185@qq.com>
2019-12-23 23:23:26 +0800

    33

--- src/main/java/com/qkl/wallet/core/event/listener/TokenTransferEventListener.java
+++ src/main/java/com/qkl/wallet/core/event/listener/TokenTransferEventListener.java
@@ -1,5 +1,5 @@
 package com.qkl.wallet.core.event.listener;
-import com.qkl.wallet.common.enumeration.CallbackTypeEnum;
+import com.alibaba.fastjson.JSON;
 import com.qkl.wallet.common.walletUtil.WalletUtils;
 import com.qkl.wallet.core.event.TokenTransferEvent;
 import com.qkl.wallet.core.manage.OrderManage;
@@ -8,7 +8,6 @@ import com.qkl.wallet.core.transfer.work.OrderWorkThread;
 import com.qkl.wallet.domain.RawTransactionResEntity;
 import com.qkl.wallet.service.WalletService;
 import com.qkl.wallet.service.impl.EventService;
-import com.qkl.wallet.vo.out.WithdrawCallback;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
@@ -46,7 +45,7 @@ public class TokenTransferEventListener extends Listener{
             RawTransactionResEntity entity = WalletUtils.offlineTransferToken(contractAddress,toAddress,amount.toBigInteger(),fromAddress);
 
             //将当前txHash缓存到redis,用于在充值回调的校验,区分是提现订单还是充值订单
-            OrderManage.addWithdrawTxHashNumber(entity.getTransactionHash(),JSON.toJSONString(loadBaseOrder(trace,orderModel.getTxnType())));
+            OrderManage.addWithdrawTxHashNumber(entity.getTransactionHash(), JSON.toJSONString(loadBaseOrder(trace,orderModel.getTxnType())));
             //由于区块链的转账规则,每次交易的nonce随机数必须是递增并且不一致的随机数,所以,这里需要等待区块链处理完毕并且更新nonce才能继续处理下一笔订单
             WalletUtils.monitorNonceIsUpdate(entity.getNonce(),fromAddress);
         } catch (Exception e) {
